2eefc9f7b6c283de2ece213f1677bfd4ae39121a
gsmolinski <g.smolinski1@gmail.com>
2021-10-05 10:36:43 +0200

    subset data if entire table not necessary

--- R/na_move.R
+++ R/na_move.R
@@ -9,5 +9,15 @@ na_move <- function(data, cols = names(data), direction = "right") {
     setDT(data)
   }
 
+  if (direction %in% c("right", "left")) {
+    cols_all <- names(data)
+    check_cols_lgl <- cols_all %in% cols
 
+    if (!all(check_cols_lgl)) { # no need to work on entire dataframe
+      data_rest <- data[, .SD, .SDcols = cols_all[!check_cols_lgl]]
+      data <- data[, .SD, .SDcols = cols_all[check_cols_lgl]]
+    }
+  } else { # "top" or "bottom"
+
+  }
 }
