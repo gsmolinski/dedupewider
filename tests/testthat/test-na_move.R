a0cd76f4ad1881b80a74bfbbf733b77193402336
gsmolinski <g.smolinski1@gmail.com>
2021-10-26 12:41:16 +0200

    simple moving works - tests added

--- tests/testthat/test-na_move.R
+++ tests/testthat/test-na_move.R
@@ -1,3 +1,43 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+df_simple <- data.frame(col1 = c(1, NA, 2, NA),
+                 col2 = c(NA, NA, NA, NA),
+                 col3 = c(1, 2, NA, NA),
+                 col4 = c(NA, NA, 1, 2))
+
+
+test_that("moving NA to right works", {
+  df_correct <- data.frame(col1 = c(1, 2, 2, 2),
+                           col2 = c(1, NA, 1, NA),
+                           col3 = c(NA, NA, NA, NA),
+                           col4 = c(NA, NA, NA, NA))
+  df_moved <- na_move(df_simple)
+  expect_equal(df_correct, df_moved)
 })
+
+test_that("moving NA to left works", {
+  df_correct <- data.frame(col1 = c(NA, NA, NA, NA),
+                           col2 = c(NA, NA, NA, NA),
+                           col3 = c(1, NA, 1, NA),
+                           col4 = c(1, 2, 2, 2))
+  df_moved <- na_move(df_simple, direction = "left")
+  expect_equal(df_correct, df_moved)
+})
+
+test_that("moving NA to top works", {
+  df_correct <- data.frame(col1 = c(NA, NA, 1, 2),
+                           col2 = c(NA, NA, NA, NA),
+                           col3 = c(NA, NA, 1, 2),
+                           col4 = c(NA, NA, 1, 2))
+  df_moved <- na_move(df_simple, direction = "top")
+  expect_equal(df_correct, df_moved)
+})
+
+test_that("moving NA to bottom works", {
+  df_correct <- data.frame(col1 = c(1, 2, NA, NA),
+                           col2 = c(NA, NA, NA, NA),
+                           col3 = c(1, 2, NA, NA),
+                           col4 = c(1, 2, NA, NA))
+  df_moved <- na_move(df_simple, direction = "bottom")
+  expect_equal(df_correct, df_moved)
+})
+
+
