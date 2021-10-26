16b6417809ab763dea116f81d0030744e2c2d0a5
gsmolinski <g.smolinski1@gmail.com>
2021-10-26 12:35:30 +0200

    initializing cols_all var moved before setDT because names() fun makes reference to object

--- R/na_move.R
+++ R/na_move.R
@@ -26,13 +26,13 @@ na_move <- function(data, cols = names(data), direction = "right") {
 
   attributes_data <- attributes(data)
   data <- copy(data)
+  cols_all <- names(data) # before setDT, because names makes reference
 
   if (!is.data.table(data)) {
     setDT(data)
   }
 
   if (direction %in% c("right", "left")) {
-    cols_all <- names(data)
     check_cols_lgl <- cols_all %in% cols
 
     if (!all(check_cols_lgl)) { # no need to work on entire dataframe
