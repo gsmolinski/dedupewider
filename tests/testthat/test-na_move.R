74ee7fa9981e372f15f3ef6b44afb591997baf98
gsmolinski <g.smolinski1@gmail.com>
2021-10-26 14:12:12 +0200

    Add tests about preserving col orders as well as testing case when row has only NA

--- tests/testthat/test-na_move.R
+++ tests/testthat/test-na_move.R
@@ -3,7 +3,6 @@ df_simple <- data.frame(col1 = c(1, NA, 2, NA),
                  col3 = c(1, 2, NA, NA),
                  col4 = c(NA, NA, 1, 2))
 
-
 test_that("moving NA to right works", {
   df_correct <- data.frame(col1 = c(1, 2, 2, 2),
                            col2 = c(1, NA, 1, NA),
@@ -40,4 +39,22 @@ test_that("moving NA to bottom works", {
   expect_equal(df_correct, df_moved)
 })
 
+test_that("cols order is preserved when moving NA to right", {
+  df_moved <- na_move(df_simple, cols = c("col2", "col3"))
+  expect_equal(names(df_simple), names(df_moved))
+  df_moved <- na_move(df_simple, cols = c("col1", "col4")) # this is also a case when given row contains only NA
+  expect_equal(names(df_simple), names(df_moved))
+})
 
+test_that("cols order is preserved when moving NA to left", {
+  df_moved <- na_move(df_simple, cols = c("col2", "col3"), direction = "left")
+  expect_equal(names(df_simple), names(df_moved))
+  df_moved <- na_move(df_simple, cols = c("col1", "col4"), direction = "left") # this is also a case when given row contains only NA
+  expect_equal(names(df_simple), names(df_moved))
+})
+
+test_that("even if row contains only NA, function works", {
+  df_simple <- df_simple[, c(1, 4)] # row 2 has only NA
+  df_moved <- na_move(df_simple)
+  expect_equal(nrow(df_simple), nrow(df_moved))
+})
